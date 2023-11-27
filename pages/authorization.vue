7723f2af87b09e072300b14422bd977796b9b1cc
zhazhazhu <fangyuan.leslie@qq.com>
2023-11-27 20:42:00 +0800

    init

---
+++ pages/authorization.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { Icon } from "@iconify/vue";
+const route = useRoute();
+const router = useRouter();
+const code = route.query.code as string;
+const setup_action = route.query.setup_action as string;
+
+onMounted(() => {
+  if (!code) return;
+  if (setup_action && setup_action === "install") {
+    location.href = "picx://installations" + location.search;
+    return;
+  }
+  location.href = "picx://authorization" + location.search;
+});
+</script>
+
+<template>
+  <div class="text-center pt-30px">
+    <Icon icon="fluent-emoji:carrot" class="text-5rem color-gray-6" />
+    <template v-if="code">
+      {{ code }}
+      <div class="text-1.5rem color-gray-6">
+        GitHub authorization authentication successful
+      </div>
+      <div class="text-1rem color-gray-4">about to open the app</div>
+    </template>
+    <template v-else>
+      <div class="text-1.5rem color-gray-6">
+        GitHub authorization authentication
+      </div>
+    </template>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div h-100vh>
+    <div class="pt-50px text-center">
+      <div class="mb-30px w-full flex flex-col items-center">
+        <img src="/login.png" width="300" />
+        <div
+          class="my-4px text-2rem font-bold lh-38px color-gray-6 base-text-color"
+        >
+          Welcome to Picx
+        </div>
+        <p class="my-0 color-gray"></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="less" scoped></style>
