1a5c8185ab8a5548ebcfeb3a56fcceeb76338af2
zhazhazhu <fangyuan.leslie@qq.com>
2023-12-13 17:50:20 +0800

    chore: change favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,8 +46,8 @@ export default defineNuxtConfig({
       viewport: 'width=device-width,initial-scale=1',
       link: [
         { rel: 'icon', href: '/favicon.ico', sizes: 'any' },
-        { rel: 'icon', type: 'image/svg+xml', href: '/nuxt.svg' },
-        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' },
+        { rel: 'apple-touch-icon', href: '/favicon.png' },
       ],
       meta: [
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
